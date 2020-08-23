ee9890d1440b132fa8f2d6298283b362fb4de39b
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2020-08-23 22:18:51 +0200

    Add constant functions

---
+++ Sources/NoopKit/Constant.generated.swift
@@ -0,0 +1,158 @@
+// Generated using Sourcery 1.0.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<T>(
+    _ value: T
+) -> () -> T {
+    {
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, T>(
+    _ value: T
+) -> (P1) -> T {
+    { _ in
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, P2, T>(
+    _ value: T
+) -> (P1, P2) -> T {
+    { _, _ in
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, P2, P3, T>(
+    _ value: T
+) -> (P1, P2, P3) -> T {
+    { _, _, _ in
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, P2, P3, P4, T>(
+    _ value: T
+) -> (P1, P2, P3, P4) -> T {
+    { _, _, _, _ in
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, P2, P3, P4, P5, T>(
+    _ value: T
+) -> (P1, P2, P3, P4, P5) -> T {
+    { _, _, _, _, _ in
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, P2, P3, P4, P5, P6, T>(
+    _ value: T
+) -> (P1, P2, P3, P4, P5, P6) -> T {
+    { _, _, _, _, _, _ in
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, P2, P3, P4, P5, P6, P7, T>(
+    _ value: T
+) -> (P1, P2, P3, P4, P5, P6, P7) -> T {
+    { _, _, _, _, _, _, _ in
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, P2, P3, P4, P5, P6, P7, P8, T>(
+    _ value: T
+) -> (P1, P2, P3, P4, P5, P6, P7, P8) -> T {
+    { _, _, _, _, _, _, _, _ in
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, P2, P3, P4, P5, P6, P7, P8, P9, T>(
+    _ value: T
+) -> (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> T {
+    { _, _, _, _, _, _, _, _, _ in
+        value
+    }
+}
+
+/// Higher-order function returning function which returns
+/// constant value.
+///
+/// - Parameter value: Constant value which will be returned.
+/// - Returns: Closure throwing away all passed parameters
+///            and returning passed value.
+public func constant<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, T>(
+    _ value: T
+) -> (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10) -> T {
+    { _, _, _, _, _, _, _, _, _, _ in
+        value
+    }
+}
+
