1c70724cb758a0ce00f14f3d6ea975992aa11c87
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2020-08-28 21:48:23 +0200

    Add id function templates and generated code

---
+++ Sources/NoopKit/Id.generated.swift
@@ -0,0 +1,204 @@
+// Generated using Sourcery 1.0.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1>(
+    _ p1: P1
+) -> (P1) {
+    (p1)
+}
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1, P2>(
+    _ p1: P1,
+    _ p2: P2
+) -> (P1, P2) {
+    (p1, p2)
+}
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1, P2, P3>(
+    _ p1: P1,
+    _ p2: P2,
+    _ p3: P3
+) -> (P1, P2, P3) {
+    (p1, p2, p3)
+}
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1, P2, P3, P4>(
+    _ p1: P1,
+    _ p2: P2,
+    _ p3: P3,
+    _ p4: P4
+) -> (P1, P2, P3, P4) {
+    (p1, p2, p3, p4)
+}
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1, P2, P3, P4, P5>(
+    _ p1: P1,
+    _ p2: P2,
+    _ p3: P3,
+    _ p4: P4,
+    _ p5: P5
+) -> (P1, P2, P3, P4, P5) {
+    (p1, p2, p3, p4, p5)
+}
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1, P2, P3, P4, P5, P6>(
+    _ p1: P1,
+    _ p2: P2,
+    _ p3: P3,
+    _ p4: P4,
+    _ p5: P5,
+    _ p6: P6
+) -> (P1, P2, P3, P4, P5, P6) {
+    (p1, p2, p3, p4, p5, p6)
+}
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1, P2, P3, P4, P5, P6, P7>(
+    _ p1: P1,
+    _ p2: P2,
+    _ p3: P3,
+    _ p4: P4,
+    _ p5: P5,
+    _ p6: P6,
+    _ p7: P7
+) -> (P1, P2, P3, P4, P5, P6, P7) {
+    (p1, p2, p3, p4, p5, p6, p7)
+}
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1, P2, P3, P4, P5, P6, P7, P8>(
+    _ p1: P1,
+    _ p2: P2,
+    _ p3: P3,
+    _ p4: P4,
+    _ p5: P5,
+    _ p6: P6,
+    _ p7: P7,
+    _ p8: P8
+) -> (P1, P2, P3, P4, P5, P6, P7, P8) {
+    (p1, p2, p3, p4, p5, p6, p7, p8)
+}
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1, P2, P3, P4, P5, P6, P7, P8, P9>(
+    _ p1: P1,
+    _ p2: P2,
+    _ p3: P3,
+    _ p4: P4,
+    _ p5: P5,
+    _ p6: P6,
+    _ p7: P7,
+    _ p8: P8,
+    _ p9: P9
+) -> (P1, P2, P3, P4, P5, P6, P7, P8, P9) {
+    (p1, p2, p3, p4, p5, p6, p7, p8, p9)
+}
+
+/// Function used for passing to higher-order functions.
+/// It returns all the passed parameters as a tuple.
+///
+/// Parameters:
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+/// - Parameter _: Parameter will be returned in a tuple.
+public func id<P1, P2, P3, P4, P5, P6, P7, P8, P9, P10>(
+    _ p1: P1,
+    _ p2: P2,
+    _ p3: P3,
+    _ p4: P4,
+    _ p5: P5,
+    _ p6: P6,
+    _ p7: P7,
+    _ p8: P8,
+    _ p9: P9,
+    _ p10: P10
+) -> (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10) {
+    (p1, p2, p3, p4, p5, p6, p7, p8, p9, p10)
+}
+
